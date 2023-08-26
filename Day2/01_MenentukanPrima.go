9aa26e0432a2da9401c17971c290a52b9ba6264b
Widi Wijaya <widiwijaya1221@gmail.com>
2023-08-26 07:12:38 +0700

    Optimasi Prima

--- Day2/01_MenentukanPrima.go
+++ Day2/01_MenentukanPrima.go
@@ -3,23 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var angka, i int
-	i = 2
+	var angka int
+	isFound := false
 
 	fmt.Print("Masukkan bilangan yg mau di cek prima = ")
 	fmt.Scanln(&angka)
 
-	for temp := 2; temp < angka; temp++ {
-		if angka%temp == 0 {
-			break
-		}
-		i++
-	}
-
-	if angka == i {
-		fmt.Println(angka, " adalah bilangan prima")
+	if angka == 2 {
+		fmt.Println(angka, "dua  bilangan prima")
+	} else if angka%2 == 0 || angka <= 1 {
+		fmt.Println(angka, "satu bukan bilangan prima")
 	} else {
-		fmt.Println(angka, " bukan bilangan prima")
+		for temp := 3; temp*temp <= angka; temp += 2 {
+			if angka%temp == 0 {
+				isFound = true
+				break
+			}
+		}
+		if isFound == false {
+			fmt.Println(angka, " adalah bilangan prima")
+		} else {
+			fmt.Println(angka, " bukan bilangan prima")
+		}
 	}
-
 }
